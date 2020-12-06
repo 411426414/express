e923014ec7584d76dca9aa9b3e4e8eeb67645ec7
411426414 <411426414@qq.com>
2020-12-06 23:44:24 +0800

    渲染登录视图

--- Routes/index.js
+++ Routes/index.js
@@ -6,6 +6,13 @@ router.get('/', function (req, res) {
   res.send('hello express');
 })
 
+router.get('/login', function (req, res) {
+  // res.send('router page');
+  // 渲染视图
+  res.render('login');
+});
+
+
 
 // 公开接口
 module.exports = router;
