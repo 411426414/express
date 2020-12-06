6dc88885426c3c67283a99b4b0198f54b15776fe
411426414 <411426414@qq.com>
2020-12-06 23:45:04 +0800

    提交登录信息使用session存储

--- Routes/index.js
+++ Routes/index.js
@@ -13,6 +13,21 @@ router.get('/login', function (req, res) {
 });
 
 
+router.post('/login', function (req, res) {
+  // res.send(req.body.username);
+  var session = req.session;
+  // 第N次登录
+  if (session.uniqueId) {
+    // 交由专门的方法处理
+    res.redirect('/redirects');
+  } else {
+    // 第一次登陆
+    session.uniqueId = req.body.username;
+    // 交由专门的方法处理
+    res.redirect('/redirects');
+  }
+})
+
 
 // 公开接口
 module.exports = router;
